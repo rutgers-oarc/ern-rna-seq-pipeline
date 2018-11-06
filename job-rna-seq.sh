1d1d20980a4b3a24987ffd84b0d21e9024e0812a
Bala Desinghu <dmbala@gmail.com>
2018-11-06 10:41:58 -0500

    added relative paths

--- job-rna-seq.sh
+++ job-rna-seq.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-cd /home/bd387/RNA-Seq-Jobs
-pwd
-ls /home/bd387/simg-repo/
 export PATH=$PATH:/usr/local/bin:.
-echo "HOSTNAME"
-echo "=================="
-hostname 
-echo "=================="
+echo "DATE" >> job-hostnames.out
+date >> job-hostnames.out
+echo "COMPUTE-NODE" >> job-hostnames.out
+echo "==================" >> job-hostnames.out
+hostname  >> job-hostnames.out
+echo "==================" >> job-hostnames.out
 echo $PATH 
-echo $PATH 
-kallisto --help
-nextflow run rna-seq-trans-align.nf -with-trace -with-report RNA-seq-nf.html  -with-timeline RNA-seq-nf-timeline.html -with-dag flowchart.png 
-rm -rf /home/bd387/RNA-Seq-Jobs/work
+nextflow run rna-seq-trans-align.nf -with-trace -with-report RNA-seq-nf.html  -with-timeline RNA-seq-nf-timeline.html
+rm -rf work
 exit
